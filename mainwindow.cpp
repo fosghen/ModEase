5d09a99db2cb88e757479419939ecca9c82db644
fosghen <r.efremov2@g.nsu.ru>
2024-11-11 16:52:52 +0700

    refact: update mainwindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,32 +1,32 @@
 #include "mainwindow.h"
-#include "settingsdialog.h"
+#include "connectionmanager.h"
+#include "registermanager.h"
+#include "tablemanager.h"
 #include "./ui_mainwindow.h"
-#include "algorithm"
+#include "settingsdialog.h"
 
-#include <QModbusRtuSerialClient>
-#include <QtCore>
-#include <QtGui>
-#include <QtWidgets>
-#include <QtSerialPort>
+#include <QFile>
+#include <QSerialPortInfo>
+#include <QModbusDevice>
 
-int current_register = 0;
-int prev_reg = 0;
-int count_click = 0;
-
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::ModEase)
-{
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::ModEase)
+ {
+    m_connectionManager = new ConnectionManager(this);
+    m_registerManager = new RegisterManager(this);
+    m_tableManager = new TableManager(this);
     m_settingsDialog = new SettingsDialog(this);
     ui->setupUi(this);
+
+    customizeWindow();
     initActions();
     defineComPorts();
-    readInitialize();
-    customizeWindow();
+    m_tableManager->readInitialize(ui->tabWidget, ui->multyBox);
+    m_registerManager->registersInit(m_tableManager);
 }
 
-void MainWindow::customizeWindow()
-{
+void MainWindow::customizeWindow() {
     this->setWindowTitle("ModEase");
     this->setWindowIcon(QIcon(":/icons/main_icon.png"));
 
@@ -34,67 +34,44 @@ void MainWindow::customizeWindow()
     if (file.open(QFile::ReadOnly | QFile::Text)) {
         QTextStream stream(&file);
         qApp->setStyleSheet(stream.readAll());
-    }
+     }
 }
 
-void MainWindow::initActions()
-{
-    connect(ui->connectButton, &QPushButton::clicked, this, &MainWindow::onConnectButtonClicked);
-    connect(ui->setDataButton, &QPushButton::clicked, this, &MainWindow::onSetDataButtonClicked);
-    connect(ui->getDataButton, &QPushButton::clicked, this, &MainWindow::onGetDataButtonClicked);
+void MainWindow::initActions() {
+    connect(m_connectionManager, &ConnectionManager::connectionStatusChanged, this, &MainWindow::updateUIOnConnectionStatus);
+    connect(m_connectionManager, &ConnectionManager::connectionFailed, this, &MainWindow::displayConnectionError);
 
+    connect(ui->connectButton, &QPushButton::clicked, this, &MainWindow::handleConnectButton);
     connect(ui->actionSettings, &QAction::triggered, m_settingsDialog, &QDialog::show);
     connect(ui->multyBox, &QCheckBox::checkStateChanged, this, &MainWindow::multyBoxChanged);
+    connect(ui->setDataButton, &QPushButton::clicked, this, &MainWindow::sendData);
+    connect(ui->getDataButton, &QPushButton::clicked, this, &MainWindow::getData);
+
+    connect(m_registerManager, &RegisterManager::clearStatusBar, this, &MainWindow::clearStatusBar);
+    connect(m_registerManager, &RegisterManager::writeError, this, &MainWindow::displayConnectionError);
+    connect(m_registerManager, &RegisterManager::isFinished, this, &MainWindow::finishGetData);
 }
 
-void MainWindow::multyBoxChanged()
+void MainWindow::sendData()
 {
-    if (!ui->multyBox->isChecked()){
-        clearTable();
-    }
+    m_registerManager->writeRegisters(m_tableManager, ui->slaveEdit->value(), m_connectionManager->getDevice());
 }
 
-void MainWindow::onConnectButtonClicked()
+void MainWindow::finishGetData()
 {
-    if (isConnected){
-        modbusDevice->disconnectDevice();
-        delete modbusDevice;
-        modbusDevice = nullptr;
+    m_tableManager->updateTable();
+    ui->getDataButton->setEnabled(true);
+}
 
-        ui->connectButton->setText("Подключиться");
-        ui->getDataButton->setEnabled(false);
-        ui->setDataButton->setEnabled(false);
+void MainWindow::getData()
+{
+    ui->getDataButton->setEnabled(false);
+    m_registerManager->readRegisters(ui->slaveEdit->value(), m_connectionManager->getDevice());
+}
 
-        defineComPorts();
-
-        isConnected = !isConnected;
-    } else{
-        modbusDevice = new QModbusRtuSerialClient(this);
-
-        const auto settings = m_settingsDialog->settings();
-
-        modbusDevice->setConnectionParameter(QModbusDevice::SerialPortNameParameter,
-                                             ui->portComboBox->currentText());
-        modbusDevice->setConnectionParameter(QModbusDevice::SerialParityParameter,
-                                             settings.parity);
-        modbusDevice->setConnectionParameter(QModbusDevice::SerialBaudRateParameter,
-                                             settings.baud);
-        modbusDevice->setConnectionParameter(QModbusDevice::SerialDataBitsParameter,
-                                             settings.dataBits);
-        modbusDevice->setConnectionParameter(QModbusDevice::SerialStopBitsParameter,
-                                             settings.stopBits);
-
-        modbusDevice->setTimeout(1000);
-        modbusDevice->setNumberOfRetries(3);
-        if (!modbusDevice->connectDevice()) {
-            statusBar()->showMessage(tr("Подключение не выполнено: %1").arg(modbusDevice->errorString()), 5000);
-        } else {
-            ui->connectButton->setText("Отключиться");
-            ui->getDataButton->setEnabled(true);
-            ui->setDataButton->setEnabled(true);
-            isConnected = !isConnected;
-        }
-    }
+void MainWindow::clearStatusBar()
+{
+    statusBar()->clearMessage();
 }
 
 void MainWindow::defineComPorts()
@@ -106,309 +83,50 @@ void MainWindow::defineComPorts()
     }
 }
 
-void MainWindow::toggleRegisterState(QTableWidget* table, int row)
-{
-    int register_ = table->item(row, 3)->text().toInt();
-
-    if (!ui->multyBox->isChecked()){
-        if (registersWrite[register_] == 1) {
-            clearTable();
-            return;
-        }
-        clearTable();
-    }
-
-    for (int col = 0; col < table->columnCount(); ++col) {
-        QTableWidgetItem *item = table->item(row, col);
-        if (item) {
-            if (!registersWrite[register_]){
-                item->setBackground(QBrush(QColor(255, 255, 0, 128)));
-            } else {
-                item->setBackground(QBrush(QColor(255, 255, 255)));
-            }
-        }
-    }
-
-    registersWrite[register_] = !registersWrite[register_];
-}
-
-void MainWindow::onCellClicked(int row, int column)
-{
-    QTableWidget *table = qobject_cast<QTableWidget*>(sender());
-    if (!table) return;
-
-    int register_ = table->item(row, 3)->text().toInt();
-    if (register_ != prev_reg) {
-        prev_reg = register_;
-        return;
-    }
-
-    toggleRegisterState(table, row);
-}
-
-void MainWindow::onEnterClicked(QTableWidget* table)
-{
-    int row = table->currentRow();
-    if (row >= 0) {
-        toggleRegisterState(table, row);
-    }
-}
-
-void MainWindow::onCellDoubleClicked(int row, int column)
-{
-    QTableWidget *table = qobject_cast<QTableWidget*>(sender());
-    if (table) {
-        toggleRegisterState(table, row);
-    }
-}
-
-void MainWindow::onSetDataButtonClicked()
-{
-    if (!modbusDevice)
-        return;
-    statusBar()->clearMessage();
-
-    for (int i = 0; i < registersAddress.size(); i++){
-        if (!registersWrite[registersAddress[i]]) continue;
-        float settedValue = registersTalbeItem[registersAddress[i]].text().toDouble();
-        settedValue = settedValue / registersMulty[registersAddress[i]] + 0.5;
-        registersValues[registersAddress[i]] = static_cast<int>(settedValue);
-
-        QModbusDataUnit writeUnit = QModbusDataUnit(QModbusDataUnit::HoldingRegisters,
-                                                    registersAddress[i],
-                                                    1);
-        writeUnit.setValue(0, registersValues[registersAddress[i]]);
-
-        if (auto *reply = modbusDevice->sendWriteRequest(writeUnit, ui->slaveEdit->value())) {
-            if (!reply->isFinished()) {
-                connect(reply, &QModbusReply::finished, this, [this, reply]() {
-                    const auto error = reply->error();
-                    if (error == QModbusDevice::ProtocolError) {
-                        statusBar()->showMessage(tr("Ошибка записи ответа: %1 (Исключение Modbus: 0x%2)")
-                                                     .arg(reply->errorString()).arg(reply->rawResult().exceptionCode(), -1, 16),
-                                                     5000);
-                    } else if (error != QModbusDevice::NoError) {
-                        statusBar()->showMessage(tr("Ошибка записи ответа: %1 (код: 0x%2)").
-                                                 arg(reply->errorString()).arg(error, -1, 16), 5000);
-                    }
-                    reply->deleteLater();
-                });
-            } else {
-                reply->deleteLater();
-            }
-        }
-    }
-    clearTable();
-}
-
-void MainWindow::onGetDataButtonClicked()
-{
-    ui->getDataButton->setEnabled(false);
-    if (!modbusDevice)
-        return;
-    statusBar()->clearMessage();
-    current_register = 0;
-
-    for (int i = 0; i < registersAddress.size(); i++){
-        if (registersAccess[registersAddress[i]] == 1) continue; // Не тестировалось
-        QModbusDataUnit readRequest = QModbusDataUnit(QModbusDataUnit::HoldingRegisters,
-                                                      registersAddress[i],
-                                                      1);
-        if (auto *reply = modbusDevice->sendReadRequest(readRequest, ui->slaveEdit->value())) {
-            if (!reply->isFinished())
-                connect(reply, &QModbusReply::finished, this, &MainWindow::onReadReady);
-            else
-                delete reply;
-        } else {
-            statusBar()->showMessage(tr("Ошибка чтения: %1").arg(modbusDevice->errorString()), 5000);
-        }
-    }
-}
-
-void MainWindow::onReadReady()
-{
-    auto reply = qobject_cast<QModbusReply *>(sender());
-    if (!reply)
-        return;
-
-    if (reply->error() == QModbusDevice::NoError) {
-        const QModbusDataUnit unit = reply->result();
-        registersValues[registersAddress[current_register]] = unit.value(0);
-        current_register++;
-        if (registersAccess[registersAddress[current_register]] == 1) current_register ++;
-    } else if (reply->error() == QModbusDevice::ProtocolError) {
-        statusBar()->showMessage(tr("Ошибка чтения ответа: %1 (Исключение Modbus: 0x%2)").
-                                 arg(reply->errorString()).
-                                 arg(reply->rawResult().exceptionCode(), -1, 16), 5000);
-    } else {
-        statusBar()->showMessage(tr("Ошибка чтения ответа: %1 (код: 0x%2)").
-                                 arg(reply->errorString()).
-                                 arg(reply->error(), -1, 16), 5000);
-    }
-
-    reply->deleteLater();
-
-    if (current_register == registersAddress.size()){
-        updateTable();
+void MainWindow::updateUIOnConnectionStatus(bool isConnected) {
+    if (isConnected) {
+        ui->connectButton->setText("Отключиться");
         ui->getDataButton->setEnabled(true);
+        ui->setDataButton->setEnabled(true);
+    } else {
+        ui->connectButton->setText("Подключиться");
+        ui->getDataButton->setEnabled(false);
+        ui->setDataButton->setEnabled(false);
     }
 }
 
-void MainWindow::updateTable()
-{
-    for (int i = 0; i < registersAddress.size(); i++){
-        float value = registersValues[registersAddress[i]];
-        value *= registersMulty[registersAddress[i]];
+void MainWindow::displayConnectionError(const QString &errorMessage) {
+    statusBar()->showMessage(errorMessage, 5000);
+}
 
-        registersTalbeItem[registersAddress[i]].setText(QString::number(value, 'f', registersDigits[registersAddress[i]]));
+void MainWindow::handleConnectButton() {
+    auto settings = m_settingsDialog->settings();
+    settings.portName = ui->portComboBox->currentText();
+    if (m_connectionManager->connectDevice(settings)) {
+        ui->connectButton->setText("Отключиться");
+    } else {
+        ui->connectButton->setText("Подключиться");
     }
 }
 
-void MainWindow::readInitialize()
+void MainWindow::multyBoxChanged()
 {
-    QString filePath = "registers.json";
-
-    readJsonData(filePath);
-    pasteJsonData();
-}
-
-void MainWindow::readJsonData(const QString jsonDataPath)
-{
-    QFile file(jsonDataPath);
-    if (!file.open(QIODevice::ReadOnly)) {
-        qDebug() << "Failed to open file";
-    }
-
-    QByteArray data = file.readAll();
-    file.close();
-
-    QJsonDocument doc = QJsonDocument::fromJson(data);
-    if (doc.isNull() || !doc.isObject()) {
-        throw std::runtime_error("Failed to parse JSON");
-    }
-
-    jsonData = doc.object();
-}
-
-void MainWindow::configItem(QTableWidgetItem* item)
-{
-    QFont *font = new QFont();
-    font->setBold(true);
-
-    item->setFont(*font);
-    item->setFlags(item->flags() & ~Qt::ItemIsEditable);
-}
-
-QTableWidget* MainWindow::configTab(QString tab, QWidget* newTab, QJsonObject regObject)
-{
-    QTableWidget *newTable = new QTableWidget(regObject.size(), N_COLS, newTab);
-    QVBoxLayout *layout = new QVBoxLayout(newTab);
-    newTable->setHorizontalHeaderLabels({"Ключевое название", "Значение", "Ед. измерения", "Адрес", "Описание"});
-    newTable->setColumnWidth(4, 200);
-    newTable->setColumnWidth(0, 150);
-    newTable->setSelectionBehavior(QAbstractItemView::SelectRows);
-    newTable->setSelectionMode(QAbstractItemView::SingleSelection);
-    layout->addWidget(newTable);
-    newTable->setStyleSheet("QTableWidget::item:selected { background-color: rgba(169, 169, 169, 128); }");
-    newTab->setLayout(layout);
-    ui->tabWidget->addTab(newTab, tab);
-
-    QShortcut *shortcut = new QShortcut(QKeySequence(Qt::Key_Return), newTable);
-
-    connect(newTable, &QTableWidget::cellClicked, this, &MainWindow::onCellClicked);
-    connect(newTable, &QTableWidget::cellDoubleClicked, this, &MainWindow::onCellDoubleClicked);
-    // Чтобы подключить шорткат для Enter надо создать лямбду функцию
-    connect(shortcut, &QShortcut::activated, this, [this, newTable]() {onEnterClicked(newTable);});
-    connect(ui->tabWidget, &QTabWidget::currentChanged, this, &MainWindow::clearTable);
-
-    return newTable;
-}
-
-int MainWindow::configTable(QJsonObject regInfo, QTableWidget* table, int row)
-{
-    QTableWidgetItem *meaning = new QTableWidgetItem(regInfo.value("meaning").toString());
-    configItem(meaning);
-
-    QTableWidgetItem *units = new QTableWidgetItem(regInfo.value("units").toString());
-    configItem(units);
-
-    QTableWidgetItem *description = new QTableWidgetItem(regInfo.value("description").toString());
-    configItem(description);
-
-    int address_ = regInfo.value("address").toInt();
-    QTableWidgetItem *address = new QTableWidgetItem(QString::number(address_));
-    configItem(address);
-
-    int access = regInfo.value("access").toInt();
-    QTableWidgetItem *value = new QTableWidgetItem(regInfo.value("").toString());
-    if (access == 0) configItem(value);
-
-    registersTalbeItem[address_] = *value;
-    table->setItem(row, 0, meaning);
-    table->setItem(row, 1, &registersTalbeItem[address_]);
-    table->setItem(row, 2, units);
-    table->setItem(row, 3, address);
-    table->setItem(row, 4, description);
-
-    return address_;
-}
-
-void MainWindow::pasteJsonData()
-{
-    for (const QString& tab : jsonData.keys())
-    {
-        QWidget *newTab = new QWidget();
-        QJsonObject regObject = jsonData.value(tab).toObject();
-        QTableWidget *newTable = configTab(tab, newTab, regObject);
-        QVector<int> keys;
-        for (const QString& reg : regObject.keys()){
-            keys.append(reg.toInt());
-        }
-
-        std::sort(keys.begin(), keys.end());
-
-        int row = 0;
-        for (int reg : keys)
-        {
-            QJsonObject regInfo = regObject.value(QString::number(reg)).toObject();
-            int address = configTable(regInfo, newTable, row);
-            float multy = regInfo.value("multy").toDouble();
-            int digits = regInfo.value("digits").toInt();
-            int access = regInfo.value("access").toInt();
-
-            registersAddress.append(address);
-            registersValues.insert(address, 0);
-            registersWrite.insert(address, false);
-            registersDigits.insert(address, digits);
-            registersMulty.insert(address, multy);
-            registersAccess.insert(address, access);
-
-            row++;
-        }
+    if (!ui->multyBox->isChecked()){
+        m_tableManager->clearTable();
     }
 }
 
-void MainWindow::clearTable()
-{
-    int pageCount = ui->tabWidget->count();
-
-    for (int i = 0; i < pageCount; i++){
-        QWidget *tab = ui->tabWidget->widget(i);
-        QVBoxLayout *layout = qobject_cast<QVBoxLayout*>(tab->layout());
-        QTableWidget *tableWidget = qobject_cast<QTableWidget*>(layout->itemAt(0)->widget());
-        for (int row = 0; row < tableWidget->rowCount(); row++){
-            for (int colomn = 0; colomn < tableWidget->columnCount(); colomn++){
-                QTableWidgetItem *item = tableWidget->item(row, colomn);
-                if (item) item->setBackground(QBrush(QColor(255, 255, 255)));
-            }
-            QTableWidgetItem *item = tableWidget->item(row, 3);
-            int address = item->text().toInt();
-            registersWrite[address] = 0;
-        }
-    }
+MainWindow::~MainWindow() {
+    delete m_connectionManager;
+    delete m_registerManager;
+    delete m_tableManager;
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
+
+
+
+
+// MainWindow::~MainWindow()
+// {
+//     delete ui;
+// }
--- mainwindow.h
+++ mainwindow.h
@@ -8,71 +8,108 @@
 #include <QTableWidget>
 #include <QJsonObject>
 
+// QT_BEGIN_NAMESPACE
+// namespace Ui {
+// class ModEase;
+// }
+
+// class QModbusClient;
+// class SettingsDialog;
+// class ConnectionManager;
+// class RegisterManager;
+// class TableManager;
+
+// QT_END_NAMESPACE
+
+// class MainWindow : public QMainWindow
+// {
+//     Q_OBJECT
+
+// public:
+//     MainWindow(QWidget *parent = nullptr);
+//     QMap<QString, QTableWidget*> tableContainer;
+//     Ui::ModEase *ui;
+//     ~MainWindow();
+
+// private:
+//     void initActions();
+//     void defineComPorts();
+//     void updateTable();
+//     void setupTable(QStringList &registers_info);
+//     void readInitialize();
+//     void pasteJsonData();
+//     void readJsonData(const QString jsonDataPath);
+//     void configItem(QTableWidgetItem* item);
+//     QTableWidget* configTab(QString tab, QWidget* newTab, QJsonObject regObject);
+//     int configTable(QJsonObject regInfo, QTableWidget* table, int row);
+//     void toggleRegisterState(QTableWidget* table, int row);
+
+
+// private slots:
+//     void onConnectButtonClicked();
+//     void onGetDataButtonClicked();
+//     void onSetDataButtonClicked();
+//     void onReadReady();
+//     void onCellClicked(int row, int column);
+//     void clearTable();
+//     void onEnterClicked(QTableWidget* table);
+//     void multyBoxChanged();
+//     void onCellDoubleClicked(int row, int column);
+//     void customizeWindow();
+
+// private:
+//     QModbusClient *modbusDevice = nullptr;
+//     SettingsDialog *m_settingsDialog = nullptr;
+
+//     QJsonObject jsonData;
+//     QVector<int> registersAddress;
+//     QMap<int, int> registersValues;
+//     QMap<int, bool> registersWrite;
+//     QMap<int, int> registersDigits;
+//     QMap<int, float> registersMulty;
+//     QMap<int, int> registersAccess;
+//     QMap<int, QTableWidgetItem> registersTalbeItem;
+//     bool isConnected = false;
+// };
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class ModEase;
 }
 
-enum HoldingRegisterAddresses {
-    TGet = 0,
-    Current = 1,
-    Voltage = 2,
-    TSet = 0
-};
-
-class QModbusClient;
+class ConnectionManager;
+class RegisterManager;
 class SettingsDialog;
-QT_END_NAMESPACE
+class TableManager;
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
-
 public:
-    MainWindow(QWidget *parent = nullptr);
-    QMap<QString, QTableWidget*> tableContainer;
-    Ui::ModEase *ui;
+    explicit MainWindow(QWidget *parent = nullptr);
+
+
     ~MainWindow();
 
 private:
+    Ui::ModEase *ui;
+    SettingsDialog *m_settingsDialog = nullptr;
+    ConnectionManager *m_connectionManager;
+    RegisterManager *m_registerManager;
+    TableManager *m_tableManager;
+
+    void customizeWindow();
     void initActions();
     void defineComPorts();
-    void updateTable();
-    void setupTable(QStringList &registers_info);
-    void readInitialize();
-    void pasteJsonData();
-    void readJsonData(const QString jsonDataPath);
-    void configItem(QTableWidgetItem* item);
-    QTableWidget* configTab(QString tab, QWidget* newTab, QJsonObject regObject);
-    int configTable(QJsonObject regInfo, QTableWidget* table, int row);
-    void toggleRegisterState(QTableWidget* table, int row);
-
 
 private slots:
-    void onConnectButtonClicked();
-    void onGetDataButtonClicked();
-    void onSetDataButtonClicked();
-    void onReadReady();
-    void onCellClicked(int row, int column);
-    void clearTable();
-    void onEnterClicked(QTableWidget* table);
+    void updateUIOnConnectionStatus(bool isConnected);
+    void displayConnectionError(const QString &errorMessage);
+    void handleConnectButton();
     void multyBoxChanged();
-    void onCellDoubleClicked(int row, int column);
-    void customizeWindow();
-
-private:
-    QModbusClient *modbusDevice = nullptr;
-    SettingsDialog *m_settingsDialog = nullptr;
-
-    QJsonObject jsonData;
-    QVector<int> registersAddress;
-    QMap<int, int> registersValues;
-    QMap<int, bool> registersWrite;
-    QMap<int, int> registersDigits;
-    QMap<int, float> registersMulty;
-    QMap<int, int> registersAccess;
-    QMap<int, QTableWidgetItem> registersTalbeItem;
-    bool isConnected = false;
+    void clearStatusBar();
+    void sendData();
+    void getData();
+    void finishGetData();
 };
 
 
